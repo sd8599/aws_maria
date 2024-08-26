ad293e44cf54f09443ccb619f532ec02a84c0469
sd8599 <sd8599@naver.com>
2024-08-26 17:34:05 +0900

    mariadb2

--- db/maria_db.sql
+++ db/maria_db.sql
@@ -14,10 +14,77 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- 테이블 데이터 sola.user_role:~3 rows (대략적) 내보내기
+
+-- sola 데이터베이스 구조 내보내기
+CREATE DATABASE IF NOT EXISTS `sola` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `sola`;
+
+-- 테이블 sola.board 구조 내보내기
+CREATE TABLE IF NOT EXISTS `board` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `title` varchar(50) NOT NULL DEFAULT '',
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_board_user_id` (`user_id`),
+  CONSTRAINT `fk_board_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='게시판';
+
+-- 테이블 데이터 sola.board:~8 rows (대략적) 내보내기
+INSERT INTO `board` (`id`, `user_id`, `title`, `content`) VALUES
+	(4, 1, 'aaa3', 'aaa3'),
+	(5, 1, 'ㅇㄻㄴㅇㄹㄴㅁㄹㅇㄴ', 'ㄴㅁㅇㄹㄴㅁㅇㄹㄴㅇㄹ'),
+	(8, 1, '뚜꿍1', '뚜꿍~'),
+	(9, 1, '뚜꿍2', '뚜꿍~'),
+	(10, 1, '뚜꿍3', '뚜꿍~'),
+	(11, 1, '야호', '홍길동'),
+	(12, 1, '헬로3', '홍길동3'),
+	(13, 1, 'dfdfdfdf', 'dfdfdfd');
+
+-- 테이블 sola.role 구조 내보내기
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='.';
+
+-- 테이블 데이터 sola.role:~2 rows (대략적) 내보내기
+INSERT INTO `role` (`id`, `name`) VALUES
+	(1, 'ROLE_USER'),
+	(2, 'ROLE_ADMIN');
+
+-- 테이블 sola.user 구조 내보내기
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `enabled` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='.';
+
+-- 테이블 데이터 sola.user:~4 rows (대략적) 내보내기
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`) VALUES
+	(1, 'aaa', '$2a$10$wG1xy.fZiB.aANQK9ep5huk1ikPse3cFMyfnVZxoTqr0ReT.cfqve', b'1'),
+	(2, 'bbb', '$2a$10$4Kk6GehGr6NLNoZPt9i1I.GPeumFjF2werUl.Ek08w0ZF9RQv2Db6', b'1'),
+	(3, 'ccc', '$2a$10$KZ5Z/YUEopqNL/9C8Q/V6.FTUC1Rr3t4i1pa7jSjpD08jLU//Gl32', b'1'),
+	(4, 'sdsd', '$2a$10$0evs.zIbHozs0eZybJP5FOsqw7JIDa7TjQ153YyQq7xFhccSO9Wai', b'1');
+
+-- 테이블 sola.user_role 구조 내보내기
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `fk_user_role_role_id` (`role_id`),
+  CONSTRAINT `fk_user_role_role_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_role_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='사용자 역할';
+
+-- 테이블 데이터 sola.user_role:~4 rows (대략적) 내보내기
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
 	(1, 1),
 	(3, 1),
+	(4, 1),
 	(1, 2);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
